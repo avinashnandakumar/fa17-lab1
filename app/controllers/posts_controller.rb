acf672b5de9d9d426a794daafdc488e0cc46e021
Avinash Nandakumar <avinashnandakumar@berkeley.edu>
2017-09-27 19:19:32 -0700

    Lab 1 Submission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,10 @@ class PostsController < ApplicationController
     # The form passes this hash to the create action, which is how the data from the form is accessed
     @post = Post.new(params[:post])
 
+
     # The SHOW action is typically synonymous to the READ in CRUD, as it usually READs data from the database
     render :show
+  end
+  def new
   end
 end
